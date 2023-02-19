a176203be9ae338106a2e0dbc0baa90f3e1d22fb
unknown <zhou7cathy@gmail.com>
2023-02-19 13:56:10 +0800

    Execute association methods on sequelize models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: 'ProductTag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: 'ProductTag',
+});
 
 module.exports = {
   Product,
